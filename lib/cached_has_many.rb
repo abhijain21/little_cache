923c0246c98966e4d0aa8f70f8c6375ccda67fb8
Abhishek Jain <abhishekjain_90@yahoo.com>
2015-03-08 17:26:22 +0530

    Update cached_has_many.rb
    add comments

--- lib/cached_has_many.rb
+++ lib/cached_has_many.rb
@@ -2,9 +2,12 @@ module HasManyExtension
 
   extend ActiveSupport::Concern
 
+  # This method generates cache key for the has_many or belongs_to association.
+  # If belongs_to_assoc param is provided, it generates for belongs_to association.
+  # This method would be called on instances of class that implement has_many.
   # Generated Cache key examples: 
-  # 1. "User_<user_id>_has_many_Comment_user_id"
-  # 2. "Post_<post_id>_has_many_Comment_user_id"
+  # 1. "User_<user_id>_has_many_Comment_<foreign_key>"
+  # 2. "Post_<post_id>_has_many_Comment_<foreign_key>"
   def get_cache_key col, belongs_to_assoc = nil
     if belongs_to_assoc
       klass_name = col.class.name
@@ -19,19 +22,28 @@ module HasManyExtension
   # add your static(class) methods here
   module ClassMethods
 
+    # Caches has_many association. The association needs to be declared using `has_many :posts` for using this method.
+    # Usage:
+    # class User < ActiveRecord::Base
+    #   has_many :posts
+    #   has_many :comments
+    #   cache_has_many :posts, :comments
+    # end
     def cache_has_many *collections
       collections.each do |col|
         define_method("cached_#{col}") do
           Rails.cache.fetch(self.get_cache_key(col), expires_in: 1.hour) do
-              # `reload` calls the SQL query and gets the association loaded. Otherwise it would do a lazy load and caching won't really happen 
+              # `reload` calls the SQL query and gets the association loaded. 
+              # Otherwise it would do a lazy load and caching won't really happen 
               self.send(col).reload
           end
         end
       end
     end
 
-
+    # Hooks callbacks to invalidate the cache whenever an association item is modified/deleted or a new one is created for a record.
     def uncache_has_many *associations
+      # Clear cache for a record for which a new association is created.
       before_update do
         associations.each do |assoc|
           obj = self.class.reflect_on_association(assoc).klass.find(self.send("#{self.class.reflect_on_association(assoc).foreign_key}_was"))
@@ -42,6 +54,7 @@ module HasManyExtension
           end
       end
       end
+      # clear cache for a record whose association is modified.
       after_commit do
         associations.each do |assoc|
           obj = self.send(assoc)
